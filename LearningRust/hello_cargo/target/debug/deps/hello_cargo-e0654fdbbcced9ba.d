fe817d2cbc6c80e6596b0bd9f52394b2b09a257e
kiranosauras <kiranbharadia@gmail.com>
2023-09-01 15:43:55 +0100

    Establishing Porjects Folder

---
+++ LearningRust/hello_cargo/target/debug/deps/hello_cargo-e0654fdbbcced9ba.d
@@ -0,0 +1,5 @@
+/Users/kiran/Documents/Projects/LearningRust/hello_cargo/target/debug/deps/hello_cargo-e0654fdbbcced9ba: src/main.rs
+
+/Users/kiran/Documents/Projects/LearningRust/hello_cargo/target/debug/deps/hello_cargo-e0654fdbbcced9ba.d: src/main.rs
+
+src/main.rs:
---
+++ LearningRust/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/kiran/Documents/Projects/LearningRust/hello_cargo/target/debug/hello_cargo: /Users/kiran/Documents/Projects/LearningRust/hello_cargo/src/main.rs
